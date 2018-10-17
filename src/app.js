23a5cc3ed436a0fb4fe0084a30115cc3d951e29b
morganafsmaia <morganafmaia@gmail.com>
2018-10-17 14:40:13 +0200

    books pages are functional and styled, missing the completion of book link json file.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,60 @@
+//Requiring packages
 const express = require('express')
+const fs = require('fs');
 const app = express()
-
-app.set('views', './src/views')
+// Setting up views and public folder
+app.set('views','src/views')
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-app.get ('/', (request, response)=>{
+// Home route --> /
+app.get('/', (request, response) => {
     response.render('home')
 })
 
+//Free books route --> /freebooks
+app.get('/freebooks', (request, response) => {
+    response.render('freebooks')
+})
 
-app.listen('3000',()=>{console.log('Listening on 3000!')})
 
+//All books route --> /books
+app.get('/books', (request, response) => {
+    response.render('allbooks')
+})
+
+//Specific books route --> /book/:name
+app.get('/book/:name', (request, response) => {
+
+    var name = request.params.name
+    fs.readFile('./book_links.json', function (err, data) {
+        if (err) {
+            throw err;
+        }
+        var parsed = JSON.parse(data);
+        for (let i = 0; i < parsed.length; i++) {
+            if (name.toLowerCase() === parsed[i].title.toLowerCase()) {
+                link = parsed[i].link
+                response.render('specificbook', {
+                    link: link
+                })
+            }
+        }
+    })
+
+})
+
+//Route to check subscription in the header link of specificbook page
+
+app.post('/specbookheader', (request, response) => {
+    var subscription = request.session.user.subscription
+    response.send(subscription);
+})
+
+
+
+
+app.listen('3000', () => {
+    console.log('Listening on 3000!')
+})
